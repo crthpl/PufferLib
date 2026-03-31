d09ecb3b34d086db2cd070c2993102d3feb2024c
Daphne Cornelisse <cor.daphne@gmail.com>
2026-03-31 18:39:19 -0400

    Typo fix.

--- ocean/drive/dataset.py
+++ ocean/drive/dataset.py
@@ -6,10 +6,10 @@ Step 0: Download the preprocessed JSON scenarios from HuggingFace e.g.,
   uv pip install huggingface_hub
   python -c "from huggingface_hub import snapshot_download; snapshot_download(repo_id='daphne-cornelisse/pufferdrive_womd_train', repo_type='dataset', local_dir='resources/drive/data')"
 
-Step 2: Unzip to get folder with .json files
+Step 1: Unzip to get folder with .json files
     unzip -j resources/drive/data/training.zip -d resources/drive/data/training/
 
-Step 1: Process to map binaries
+Step 2: Process to map binaries
   python ocean/drive/dataset.py --data_folder resources/drive/data/training --output_dir resources/drive/binaries/training
 """
 
